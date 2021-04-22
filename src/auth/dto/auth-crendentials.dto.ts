df44727fd1c13268c27e6ba2b86a9373f271c49f
Francis Badasu <nytro04@gmail.com>
2021-04-22 03:46:00 +0000

    feat: add user sign controller, services and repository with validation

--- src/auth/dto/auth-crendentials.dto.ts
+++ src/auth/dto/auth-crendentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(30)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain uppercase and lower letters and wild characters',
+  })
   password: string;
 }
